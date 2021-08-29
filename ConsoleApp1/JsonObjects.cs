8c022a24aaa3c965d132c7f10d27355278776e84
jesi2209 <jesi2209@hotmail.com>
2021-08-29 20:58:40 +0200

    Inital Push

---
+++ ConsoleApp1/JsonObjects.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+   // Root myDeserializedClass = JsonConvert.DeserializeObject<Root>(myJsonResponse);
+    public class Station
+    {
+        public string station_id { get; set; }
+        public string name { get; set; }
+        public int is_installed { get; set; }
+        public int is_renting { get; set; }
+        public int is_returning { get; set; }
+        public int last_reported { get; set; }
+        public int num_bikes_available { get; set; }
+        public int num_docks_available { get; set; }
+        override
+        public string ToString()
+        {
+            return $"Id:{station_id} Bikes:{num_bikes_available} Docks:{num_docks_available} Name:{name}\n";
+        }
+    }
+
+    public class Data
+    {
+        public List<Station> stations { get; set; }
+    }
+
+    public class Root
+    {
+        public int last_updated { get; set; }
+        public int ttl { get; set; }
+        public string version { get; set; }
+        public Data data { get; set; }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+
+        static readonly HttpClient client = new HttpClient();
+        static async Task Main(string[] args)
+        {
+            List<Station> stations = new List<Station>();
+            List<Station> tempStations = new List<Station>();
+            try
+            {
+                string response = await client.GetStringAsync("https://gbfs.urbansharing.com/oslobysykkel.no/station_status.json");               
+                Root StationStatus = JsonConvert.DeserializeObject<Root>(response);
+                stations = StationStatus.data.stations;
+
+                response = await client.GetStringAsync("https://gbfs.urbansharing.com/oslobysykkel.no/station_information.json");
+                Root StationData = JsonConvert.DeserializeObject<Root>(response);
+                tempStations = StationData.data.stations;
+
+                
+
+                foreach (var i in stations)
+                {
+                    foreach (var t in tempStations)
+                    {
+                        if (t.station_id == i.station_id){
+                            i.name = t.name;
+                        }
+                    }
+                }
+
+
+                stations.ForEach(result => Console.WriteLine(result.ToString()));
+
+
+            }
+            catch (HttpRequestException e)
+            {
+                Console.WriteLine("\nException Caught!");
+                Console.WriteLine("Message :{0} ", e.Message);
+            }
+        }
+    }
+}
